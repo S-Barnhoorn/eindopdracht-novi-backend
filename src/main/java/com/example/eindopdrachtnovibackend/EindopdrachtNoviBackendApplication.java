609aa5dcceaa02ce317d61dd4356f0089baf2e6f
Albatroaz7 <sambarnhoorn@gmail.com>
2021-06-11 10:43:58 +0200

    started project, added application properties

---
+++ src/main/java/com/example/eindopdrachtnovibackend/EindopdrachtNoviBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.eindopdrachtnovibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EindopdrachtNoviBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EindopdrachtNoviBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/eindopdrachtnovibackend/EindopdrachtNoviBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eindopdrachtnovibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EindopdrachtNoviBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
